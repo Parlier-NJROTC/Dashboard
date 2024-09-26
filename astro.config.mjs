ef0ccf45cb7f1d75489e98a75b31771439838783
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2024-09-25 18:31:44 -0700

    ooo this works

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site:"https://parlier-njrotc.github.io/Dashboard/",
-    base:"Dashboard/"
+    base:"Dashboard/",
+    experimental:{
+        env:{
+            schema:{
+                API_URL:envField.string({
+                    context:"client",
+                    access:"public"
+                })
+            }
+
+        }
+    }
 });
